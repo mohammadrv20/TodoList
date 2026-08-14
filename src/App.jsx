5a2c7ef56d651c3b91ee6ed1b47470e0b828bed7
Mohammad Hassani <mohammad.rv19@gmail.com>
2026-08-14 10:42:40 +0330

    feat : add the basics of state

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App(){
+
+  const [tasks , setTasks] = useState(["Ali" , "Javad" , "Mamad"])
+
   return(
     <>
     <h1>To Do List App</h1>
     <input type="text" placeholder="Type the Task here ..." />
     <button>Add</button>
-    <ul>
-      <li>Test</li>
-      <li>Test</li>
-      <li>Test</li>
+    <ul >
+      {tasks.map((task , index) => <li key={index}>{task}</li>)}
     </ul>
     </>
   )
