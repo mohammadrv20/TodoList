d0782b0e10b4b105989e9f60f06e5701c10c5de9
Mohammad Hassani <mohammad.rv19@gmail.com>
2026-08-14 10:39:03 +0330

    feat : add input button and lists to project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import './App.css'
 
 export default function App(){
   return(
-    <h1>The test</h1>
+    <>
+    <h1>To Do List App</h1>
+    <input type="text" placeholder="Type the Task here ..." />
+    <button>Add</button>
+    <ul>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+    </ul>
+    </>
   )
 }
