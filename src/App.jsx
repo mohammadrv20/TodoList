1b3e5e6e8f1c850c46128e033b5fc7a21aefb558
Mohammad Hassani <mohammad.rv19@gmail.com>
2026-08-14 19:48:21 +0330

    implement the up button

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,29 @@ export default function App(){
     setTasks(tasks.filter((_ , indexToRemove ) => index !== indexToRemove))
   }
 
+  function handleUpp(index){
+
+    const updateArr = [...tasks]
+    const temp = updateArr[index]
+    updateArr[index] = updateArr[index - 1]
+    updateArr[index -1] = temp
+
+    setTasks(updateArr)
+  }
+
+    console.log(tasks)
   return(
     <>
     <h1>To Do List App</h1>
     <input type="text" placeholder="Type the Task here ..." onChange={handleChange} value={task} />
     <button onClick={handleClick}>Add</button>
     <ul >
-      {tasks.map((task , index) => <li key={index}>{task} <button onClick={() => handleRemove(index)} className='text-red-600'>remove</button></li>)}
+      {tasks.map((task , index) => <li key={index}>{task}
+         <button onClick={() => handleRemove(index)}
+          className='text-red-600'>remove</button>
+          <button onClick={() => handleUpp(index)} className='text-blue-600'>Up</button>
+          <button className='text-blue-600'>Down</button>
+          </li>)}
     </ul>
     </>
   )
