b70165220283ef0de74a55e3c213bf902ee05786
Mohammad Hassani <mohammad.rv19@gmail.com>
2026-08-14 11:23:06 +0330

    feat :implement form to add new tasks to the list

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import './App.css'
 
 export default function App(){
 
-  const [tasks , setTasks] = useState(["Ali" , "Javad" , "Mamad"])
+  const [tasks , setTasks] = useState([])
+  const [task , setTask] = useState("")
+  
+  function handleChange(event){
+    setTask(event.target.value)
+  }
+  
+  function handleClick(){
+    setTasks([...tasks , task])
+  }
 
   return(
     <>
     <h1>To Do List App</h1>
-    <input type="text" placeholder="Type the Task here ..." />
-    <button>Add</button>
+    <input type="text" placeholder="Type the Task here ..." onChange={handleChange} value={task} />
+    <button onClick={handleClick}>Add</button>
     <ul >
       {tasks.map((task , index) => <li key={index}>{task}</li>)}
     </ul>
